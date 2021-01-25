cae69111fda8544fd653de118c87511e9fcd3516
KyotodevIndie <kyotodevindie@gmail.com>
2021-01-25 12:42:41 -0300

    Add some config

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ export default function Nav () {
     <nav className="nav">
       <Row justify="center">
         <Col>
-          <h1>Hero</h1>
+          <img src="https://lh3.googleusercontent.com/pw/ACtC-3elfTac962QeHLFVDSP5moJp58vig6ZmthCI78omEjxd5kNpinQC8eKcvbqkGfxynLFGSPPMTvgHAq3JCxcoQ3vOJPHZ-3tZsCKTUukTILzr0tSJW1MnDz1bPLO-R_jeDQUjGkbP3-p0yUAScdL5W0u=w54-h61-no?authuser=0" alt="logo" />
         </Col>
       </Row>
     </nav>
